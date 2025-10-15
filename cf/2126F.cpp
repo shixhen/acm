0f1f128ba4834c2cf701c4ef1bf7e8cb032b0092
拾痕 <100993123+shixhen@users.noreply.github.com>
2025-10-15 20:20:43 +0800

    Update 2126F.cpp

--- cf/2126F.cpp
+++ cf/2126F.cpp
@@ -2,9 +2,30 @@
 
 using namespace std;
 
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i];
+    long long ans = 0;
+    vector<vector<int>> edge(n + 1, vector<int>());
+    for (int i = 1, u, v; i < n; i++) {
+        cin >> u >> v;
+        edge[u].push_back(v);
+        edge[v].push_back(u);
+    }
+
+    return;
+}
+
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
-
+    int _;
+    cin >> _;
+    while (_--) {
+        solve();
+    }
     return 0;
 }
