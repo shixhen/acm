c921203844eb1f9c97a5751004f7d446f8a6687b
拾痕 <100993123+shixhen@users.noreply.github.com>
2025-11-18 13:53:41 +0800

    Add solutions for cf1064 D and E problems
    Added new source files d.cpp and e.cpp under cf/competition/cf1064, implementing solutions for problems D and E respectively.

---
+++ cf/competition/cf1064/d.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+constexpr long long mod =  998244353;
+
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<long long> a(n + 1);
+    vector<long long> cnt(5005);
+    vector<long long> dp(n + 1);
+    long long maxn = 0;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        maxn = max(maxn, ++cnt[a[i]]);
+    }
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        if (cnt[i] == 0) continue;
+        for (int j = n; j >= cnt[i]; j--) {
+            dp[j] = (dp[j] + dp[j - cnt[i]] * cnt[i]) % mod;
+        } 
+    }
+    long long ans = 0;
+    for (int i = maxn; i <= n; i++) {
+        ans = (ans + dp[i]) % mod;
+    }
+    cout << ans << "\n";
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int _;
+    cin >> _;
+    while (_--) {
+        solve();
+    }
+
+    return 0;
+}
---
+++ cf/competition/cf1064/e.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    vector<int> a(n + 1);
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+    sort(a.begin() + 1, a.begin() + n + 1, greater<int>());
+    int c;
+    
+    while (q--) {
+        cin >> c;
+        vector<int> b;
+        for (int i = 29; i >= 0; i--) {
+            if (c & (1 << i)) {
+                b.push_back(1 << i);
+            }
+        }
+        priority_queue<int> q;
+        for (int i = 1; i <= min((int) b.size(), n); i++) {
+            q.push(a[i]);
+        }
+        for (auto &x : b) {
+            int cur = q.top();
+            q.pop();
+            //cout << cur << " " << x << "\n";
+            c -= min(cur, x);
+            q.push(max(0, cur - x));
+        }
+        cout << c << "\n";
+    }
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
