9a8717760db603399a03d036eefb1b0b2b487683
拾痕 <2994344891@qq.com>
2025-11-26 00:11:38 +0800

    Fix method declarations in AnimalShelf class for dequeueDog and dequeueCat

--- tests/30.cpp
+++ tests/30.cpp
@@ -4,102 +4,102 @@ using namespace std;
 
 class AnimalShelf {
 private:
-	int *dog_number;
-	int *cat_number;
-	int dog_size;
-	int cat_size;
-	int dogl;
-	int catl;
+    queue<int> dog_queue;   // dog队列
+    queue<int> cat_queue;   // cat队列
+
 public:
-	AnimalShelf() {
-		dog_size = 0;
-		cat_size = 0;
-		dogl = 0;
-		catl = 0;
-		dog_number = new int[1005];
-		cat_number = new int[1005];
-	}
-	void enqueue(int number, string type) {
-		if (type[0] == 'd') {
-			dog_number[dog_size++] = number;
-		} else {
-			cat_number[cat_size++] = number;
-		}
-	}
-	
-	void dequeueAny(int* number, string* type) {
-		if (dogl < dog_size && (catl >= cat_size || dog_number[dogl] < cat_number[catl])) {
-			number[0] = dog_number[dogl++];
-			type[0] = "dog";
-		} else if (catl < cat_size) {
-			number[0] = cat_number[catl++];
-			type[0] = "cat";
-		} else {
-			number[0] = -1;
-			type[0] = "none";
-		}
-	}
-	int dequeueDog() {
-		if (dogl < dog_size) {
-			return dog_number[dogl++];
-		} else {
-			return -1;
-		}
-	}
-	
-	int dequeueCat() {
-		if (catl < cat_size) {
-			return cat_number[catl++];
-		} else {
-			return -1;
-		}
-	}
+    AnimalShelf() {}
+
+    void enqueue(int number, string type) {
+        if (type[0] == 'd') {
+            dog_queue.push(number);
+        } else {
+            cat_queue.push(number);
+        }
+    }
+    
+    void dequeueAny(int* number, string* type) {
+        // 有队列为空的情况
+        if (dog_queue.empty() && cat_queue.empty()) {
+            number[0] = -1;
+            type[0] = "none";
+            return;
+        } else if (dog_queue.empty()) {
+            number[0] = cat_queue.front();
+            cat_queue.pop();
+            type[0] = "cat";
+            return;
+        } else if (cat_queue.empty()) {
+            number[0] = dog_queue.front();
+            dog_queue.pop();
+            type[0] = "dog";
+            return;
+        }
+        // 两个队列都不为空，比较队头元素
+        if (dog_queue.front() < cat_queue.front()) {
+            number[0] = dog_queue.front();
+            dog_queue.pop();
+            type[0] = "dog";
+        } else {
+            number[0] = cat_queue.front();
+            cat_queue.pop();
+            type[0] = "cat";
+        }
+    }
+
+    int dequeueDog() {
+        if (!dog_queue.empty()) {
+            int num = dog_queue.front();
+            dog_queue.pop();
+            return num;
+        } else {
+            return -1;
+        }
+    }
+    
+    int dequeueCat() {
+        if (!cat_queue.empty()) {
+            int num = cat_queue.front();
+            cat_queue.pop();
+            return num;
+        } else {
+            return -1;
+        }
+    }
 };
 
 void solve() {
-	int n;
-	cin >> n;
-	AnimalShelf a;
-	for(int i = 1; i <= n; i++) {
-		string s;
-		cin >> s;
-		if (s == "enqueue") {
-			int x;
-			string na;
-			cin >> x >> na;
-			a.enqueue(x, na); 
-		} else if (s == "dequeueAny") {
-			int *number = new int[1];
-			string *type = new string[1];
-			a.dequeueAny(number, type);
-			cout << "[" << number[0] << " " << type[0] << "] ";
-		} else if (s == "dequeueDog") {
-			int x = a.dequeueDog();
-			if (x != -1) {
-				cout << "[" << x << " dog" << "] ";
-			} else {
-				cout << "[-1 none] ";
-			}
-		} else if (s == "dequeueCat") {
-			int x = a.dequeueCat();
-			if (x != -1) {
-				cout << "[" << x << " cat" << "] ";
-			} else {
-				cout << "[-1 none] ";
-			}
-		}
-	}
-	cout << "\n"; 
-	return;
+    int n;          // 操作次数
+    cin >> n;
+    int number;     // 编号
+    string op;      // 操作类型
+    string type;    // 动物种类
+    AnimalShelf a;
+    while (n--) {
+        cin >> op;
+        if (op == "enqueue") {
+            cin >> number >> type;
+            a.enqueue(number, type);
+            continue;
+        } else if (op == "dequeueAny") {
+            a.dequeueAny(&number, &type);
+        } else if (op == "dequeueDog") {
+            number = a.dequeueDog();
+            type = (number == -1 ? "none" : "dog");
+        } else if (op == "dequeueCat") {
+            number = a.dequeueCat();
+            type = (number == -1 ? "none" : "cat");
+        }
+        cout << "[" << number << " " << type << "] ";
+    }
+    cout << "\n";
+    return;
 }
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
-	int _ = 1;
-	cin >> _;
-	while (_--) {
-		solve();
-	} 
-	return 0;
-} 
+    int _;
+    cin >> _;
+    while (_--) {
+        solve();
+    }
+}
