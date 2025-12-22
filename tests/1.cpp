4cfcbbeaabc0be2002fbe2eb4be97a0c8a8460eb
拾痕 <2994344891@qq.com>
2025-12-23 00:32:51 +0800

    Implement various data structures and algorithms including segment trees, AVL trees, and a custom animal shelter queue. Enhance functionality with lazy propagation in segment trees, and support for quaternary number addition. Add SPFA algorithm for shortest path detection in graphs.

--- tests/1.cpp
+++ tests/1.cpp
@@ -1,21 +1,29 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-inline int read() {
-    int ans = 0;
-    char ch;
-    ch = getchar();
-    while(!isdigit(ch)) ch = getchar();
-    while(isdigit(ch)) {
-        ans = (ans << 3) + (ans << 1) + (ch ^ 48);
-        ch = getchar();
-    }
-    return ans;
-}
+
+const int N = 1000000 + 5;
+
+int a[N];
+int b[N];
+
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(NULL), cout.tie(NULL);
-    int t = read();
-    cout << t;
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    a[1] = 1;
+    a[2] = 2;
+    b[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        a[i] = a[i - 1] + a[i - 2] + b[i - 1];
+        
+        b[i] = b[i - 1] + a[i - 2] * 2;
+
+        a[i] %= 10000;
+        b[i] %= 10000;
+    }
+    cout << a[n] << "\n";
     return 0;
 }
