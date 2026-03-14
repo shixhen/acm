5d2ebf623fd34c3d189529849841bcedf3023cd6
拾痕 <2994344891@qq.com>
2026-03-14 19:55:42 +0800

    Add initial implementation files a.cpp, b.cpp, c.cpp, d.cpp, and e.cpp for problem set abc449

---
+++ atcoder/abc449/c.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    
+    return;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
